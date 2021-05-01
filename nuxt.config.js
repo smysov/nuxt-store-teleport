ccec3f876b334f157dc6a2200eda73c54bac6a5d
Sergey Mysov <s.mysov@list.ru>
2021-05-01 17:18:55 +0400

    init commit

---
+++ nuxt.config.js
@@ -0,0 +1,45 @@
+export default {
+  head: {
+    title: 'fortnite',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  loading: { color: 'green' },
+
+  css: ['@/assets/styles/main.scss'],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: ['@nuxtjs/eslint-module', '@nuxtjs/svg-sprite'],
+
+  modules: ['@nuxtjs/axios', '@nuxtjs/style-resources'],
+
+  axios: {},
+
+  styleResources: {
+    scss: [
+      '@/assets/styles/import/_variables.scss',
+      '@/assets/styles/import/_mixins.scss',
+    ],
+  },
+
+  svgSprite: {},
+
+  build: {
+    babel: {
+      plugins: [
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-private-methods',
+      ],
+    },
+  },
+};
